4cc990b4a2fd6e62a3ec4fd8221d76366db0e8c9
adrinjalali <adrin.jalali@gmail.com>
2023-06-19 15:51:04 +0200

    ...

--- build_tools/get_comment.py
+++ build_tools/get_comment.py
@@ -3,7 +3,6 @@
 # This script fails if there are not comments to be posted.
 
 import os
-import warnings
 import requests
 
 
@@ -34,9 +33,6 @@ def get_step_message(log, start, end, title, message):
     """
     if end not in log:
         return ""
-    
-
-    
     return (
         "-----------------------------------------------\n"
         + f"### {title}\n\n"
